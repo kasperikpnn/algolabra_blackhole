32e1daf2e8f41b981a6a8563811995423837be2b
kasperikpnn <kasperikpnn@gmail.com>
2025-03-21 14:59:03 +0200

    tekoälyn kehitys alkanut, tällä hetkellä valitsee vain satunnaisen option

---
+++ blackhole_ai.py
@@ -0,0 +1,11 @@
+import random
+
+def process_turn(board, turn_number, ai_numbers):
+    possible_positions = []
+    for i in range(21): # 21 ruutua
+        if board[i] == None:
+            possible_positions.append(i)
+    choice = random.choice(possible_positions)
+    return ai_numbers[0], choice
+    
+        
