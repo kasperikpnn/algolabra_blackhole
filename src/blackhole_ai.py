a25ca23e4803b2b79b092ff7b6bb097caac38428
kasperikpnn <kasperikpnn@gmail.com>
2025-05-21 17:24:49 +0300

    uusi, parempi heuristiikka löytynyt (viime hetkellä deadlinepäivänä)

--- src/blackhole_ai.py
+++ src/blackhole_ai.py
@@ -6,34 +6,12 @@ class BlackHoleAI:
         self.board = [None] * 21
         self.best_moves = {} # Avaimena lauta tuplena, arvona paras siirto. Käytetään siirtojen järjestämisessä
 
-    def create_sum_list(self, board):
-        """Arvioi pelitilanteen. Heuristiikkana tekoälyn senhetkisten voittoruutujen määrä miinus pelaajan senhetkisten voittoruutujen määrä."""
-        sumlist = []
-        for i in range(21):
-            if board[i] is None:
-                score = self.compute_score(i, board)
-                sumlist.append(score)
-            else:
-                sumlist.append(None)
-        return sumlist
-
-    def compute_score(self, black_hole_pos, board):
-        """Laskee kummankin pelaajan summan yksittäisen ruudun ympärillä."""
-        adjacent = adjacency[black_hole_pos]
-        player_sums = {"P1": 0, "AI": 0}
-        for pos in adjacent:
-            if board[pos]:
-                num, player = board[pos]
-                player_sums[player] += num
-        # positiivinen summa = pelaajalla on suurempi summa, negatiivinen summa = tekoälyllä on suurempi summa
-        return player_sums["P1"]-player_sums["AI"]
-
     def process_turn(self, board, ai_number, player_number):
         """Kopioi pelitilanteen ennen siirtojen kokeilua."""
         self.board = board.copy()
-        sums = self.create_sum_list(self.board)
         empty_spaces, neighbors = self.obtain_info() 
-        return self.iterative_deepening(ai_number, player_number, empty_spaces, sums, neighbors, len(empty_spaces))
+        score = self.calculate_score(board, neighbors)
+        return self.iterative_deepening(ai_number, player_number, empty_spaces, neighbors, score, len(empty_spaces))
 
     def obtain_info(self):
         """Käytetään minimaxin alussa, muodostaa listan mahdollisista siirroista ja ruutujen naapurien määrästä."""
@@ -42,34 +20,34 @@ class BlackHoleAI:
         for i in range(21): # 21 ruutua
             if self.board[i] is None:
                 empty_spaces.append(i)
-            else:
-                neighbors[i] = None
         for i in range(21):
             if self.board[i] is not None:
                 for adj_space in adjacency[i]:
-                    if neighbors[adj_space] is not None:
-                        neighbors[adj_space]-=1
+                    neighbors[adj_space]-=1
         return empty_spaces, neighbors
     
-    def evaluate(self, sums, ai_number, player_number):
-        """Arvioi pelitilanteen. Heuristiikkana tekoälyn senhetkisten voittoruutujen määrä miinus pelaajan senhetkisten voittoruutujen määrä."""
-        # Ruudut, joissa on ylivoimaisesti suurempi summa (suurempi kuin vastapelaajan seuraava numero) lasketaan kolminkertaisesti.
-        only_int_sums = [x for x in sums if x is not None]
-        return 2 * sum(x > ai_number for x in only_int_sums) + sum(x > 0 for x in only_int_sums) - sum(x < 0 for x in only_int_sums) - 2 * sum(x < -player_number for x in only_int_sums)
+    def calculate_score(self, board, neighbors):
+        score = 0
+        for i in range(21):
+            if board[i] is not None:
+                if neighbors[i] == 0:
+                    num, player = board[i]
+                    score += num if player == 'AI' else -num
+        return score
 
-    def minimax(self, depth, alpha, beta, is_maximizing, ai_number, player_number, empty_spaces, sums, neighbors):
+    def minimax(self, depth, alpha, beta, is_maximizing, ai_number, player_number, empty_spaces, neighbors, score):
         """Minimax-algoritmi tehostettuna alpha-beta -karsinnalla ja parhaan siirron muistamisella."""
         key = tuple(self.board)
         # Kun musta aukko on ainoastaan jäljellä
         if len(empty_spaces) == 1:
-            if sums[empty_spaces[0]] >= 1:
+            if score >= 1:
                 return (None, 1000) # Voitto
-            elif sums[empty_spaces[0]] == 0:
+            elif score == 0:
                 return (None, 0) # Tasapeli
             else:
                 return (None, -1000) # Häviö
         if depth == 0:
-            return (None, self.evaluate(sums, ai_number, player_number))
+            return (None, score)
         if is_maximizing: # maksimointi
             value = -math.inf
             best_space = None
@@ -83,19 +61,21 @@ class BlackHoleAI:
                 sorted_empty_spaces.insert(0, prev_best_move)
             for space in sorted_empty_spaces:
                 # Kopioidaan kaikki käsiteltävät listat
-                new_sums = sums.copy()
                 new_neighbors = neighbors.copy()
                 new_empty_spaces = empty_spaces.copy()
-                # Päivitetään summa- ja naapurilistaa kokeiltavalle siirrolle
+                new_score = score
+                # Päivitetään naapurilistaa ja pisteytystä kokeiltavalle siirrolle
                 for adj_space in adjacency[space]:
-                    if new_sums[adj_space] != None:
-                        new_sums[adj_space]-=ai_number
-                        new_neighbors[adj_space]-=1
-                new_sums[space] = None
-                new_neighbors[space] = None
+                    new_neighbors[adj_space]-=1
+                    if new_neighbors[adj_space] == 0:
+                        if self.board[adj_space] is not None:
+                            num, player = self.board[adj_space]
+                            new_score += num if player == 'AI' else -num
                 new_empty_spaces.remove(space)
                 self.board[space] = (ai_number, "AI") # tekoäly kokeilee siirtoa
-                new_score = self.minimax(depth-1, alpha, beta, False, ai_number+1, player_number, new_empty_spaces, new_sums, new_neighbors)
+                if new_neighbors[space] == 0:
+                    new_score += ai_number
+                new_score = self.minimax(depth-1, alpha, beta, False, ai_number+1, player_number, new_empty_spaces, new_neighbors, new_score)
                 self.board[space] = None # perutaan siirto
                 if new_score[1] > value:
                     value = new_score[1]
@@ -103,9 +83,6 @@ class BlackHoleAI:
                 elif new_score[1] == value:
                     if neighbors[space] < neighbors[best_space]:
                         # Vähemmän naapureita yhtä arvokkaalla siirrolla -> valitaan tämä siirto
-                        best_space = space
-                    elif sums[space] > sums[best_space]:
-                        # Jos yhtä arvokas siirto peittää ruudun jolla on suurempi summa -> valitaan tämä siirto
                         best_space = space
                 alpha = max(alpha, value)
                 if alpha > beta:
@@ -126,19 +103,21 @@ class BlackHoleAI:
                 sorted_empty_spaces.insert(0, prev_best_move)
             for space in sorted_empty_spaces:
                 # Kopioidaan kaikki käsiteltävät listat
-                new_sums = sums.copy()
                 new_neighbors = neighbors.copy()
                 new_empty_spaces = empty_spaces.copy()
-                # Päivitetään summa- ja naapurilistaa kokeiltavalle siirrolle
+                new_score = score
+                # Päivitetään naapurilistaa ja pisteytystä kokeiltavalle siirrolle
                 for adj_space in adjacency[space]:
-                    if new_sums[adj_space] != None:
-                        new_sums[adj_space]+=player_number
-                        new_neighbors[adj_space]-=1
-                new_sums[space] = None
-                new_neighbors[space] = None
+                    new_neighbors[adj_space]-=1
+                    if new_neighbors[adj_space] == 0:
+                        if self.board[adj_space] is not None:
+                            num, player = self.board[adj_space]
+                            new_score += num if player == 'AI' else -num
                 new_empty_spaces.remove(space)
                 self.board[space] = (player_number, "P1") # pelaaja kokeilee siirtoa
-                new_score = self.minimax(depth-1, alpha, beta, True, ai_number, player_number+1, new_empty_spaces, new_sums, new_neighbors)
+                if new_neighbors[space] == 0:
+                    new_score -= player_number
+                new_score = self.minimax(depth-1, alpha, beta, True, ai_number, player_number+1, new_empty_spaces, new_neighbors, new_score)
                 self.board[space] = None # perutaan siirto
                 if new_score[1] < value:
                     value = new_score[1]
@@ -147,9 +126,6 @@ class BlackHoleAI:
                     if neighbors[space] < neighbors[best_space]:
                         # Vähemmän naapureita yhtä arvokkaalla siirrolla -> valitaan tämä siirto
                         best_space = space
-                    elif sums[space] < sums[best_space]:
-                        # Jos yhtä arvokas siirto peittää ruudun jolla on suurempi summa -> valitaan tämä siirto
-                        best_space = space
                 beta = min(beta, value)
                 if alpha > beta:
                     # alpha-beta katkaisu tapahtuu
@@ -157,7 +133,7 @@ class BlackHoleAI:
             self.best_moves[key] = best_space
             return best_space, value
 
-    def iterative_deepening(self, ai_number, player_number, empty_spaces, sums, neighbors, max_depth=20, time_limit=2.0):
+    def iterative_deepening(self, ai_number, player_number, empty_spaces, neighbors, score, max_depth=20, time_limit=2.0):
         """Iteratiivinen syveneminen. Aikarajaksi asetettu 2 sekuntia."""
         best_space = None
         start_time = time.time()
@@ -165,10 +141,10 @@ class BlackHoleAI:
         for depth in range(1, max_depth+1):
             if time.time() - start_time > time_limit:
                 # ei tiukkaa aikakatkaisua: kunhan uuden syvyyden tutkiminen aloitetaan ennen kuin aikaraja menee umpeen, se sallitaan
-                print("päästiin syvyyteen " + str(depth))
+                # debug: print("päästiin syvyyteen " + str(depth-1))
                 break
-            space, value = self.minimax(depth, -math.inf, math.inf, True, ai_number, player_number, empty_spaces, sums, neighbors)
-            print("syvyys: " + str(depth) + ", paras siirto: " + str(space) + ", arvo: " + str(value))
+            space, value = self.minimax(depth, -math.inf, math.inf, True, ai_number, player_number, empty_spaces, neighbors, score)
+            # debug: print("syvyys: " + str(depth) + ", paras siirto: " + str(space) + ", arvo: " + str(value))
             if space is not None:
                 best_space = space
         return best_space, value
--- src/tests/blackhole_ai_test.py
+++ src/tests/blackhole_ai_test.py
@@ -8,18 +8,6 @@ class TestBlackHoleAI(unittest.TestCase):
     def reset_ai(self):
         self.bh_ai = blackhole_ai.BlackHoleAI()
 
-    def test_create_sum_list(self):
-        test_board = [
-            None,
-            None, (2, "P1"),
-            (2, "AI"), (1, "AI"), None,
-            (3, "P1"), None, None, None,
-            None, (1, "P1"), (3, "AI"), None, (4, "P1"),
-            None, None, (5, "AI"), None, (4, "AI"), (5, "P1")
-        ]
-        test_sums = [2, -1, None, None, None, 1, None, -2, -4, 4, 4, None, None, -3, None, 0, -4, None, -12, None, None]
-        self.assertEqual(self.bh_ai.create_sum_list(test_board), test_sums)
-
     def test_valid_move_on_empty_board(self):
         board = [None] * 21
         self.reset_ai()
@@ -39,19 +27,19 @@ class TestBlackHoleAI(unittest.TestCase):
         self.assertEqual(value, -1000)
 
     def test_minimax_win(self):
-        board = [(100, "P1")] * 19 + [None] * 2
+        board = [None] + [(5, "P1")] + [(4, "P1")] + [(3, "AI")] + [(3, "P1")] + [(2, "P1")] + [(4, "AI")] + [(2, "AI")] + [(1, "AI")] + [(1, "P1")] + [(5, "AI")] + [(6, "AI")] + [(7, "AI")] + [(7, "P1")] + [(8, "P1")] + [(8, "AI")] + [(9, "P1")] + [(4, "AI")] + [(6, "P1")] + [None] * 2
         self.reset_ai()
         move, value = self.bh_ai.process_turn(board, 10, 10)
         self.assertEqual(value, 1000)
     
     def test_minimax_tie_over_loss(self):
-        board =  [None] + [(10, "AI")] * 2 + [(10, "P1")] * 7 + [(10, "AI")] * 2 + [(10, "P1")] * 4 + [None] + [(10, "P1")] * 4
+        board = [None] + [(1, "AI")] + [(2, "AI")] + [(5, "AI")] + [(6, "AI")] + [(7, "AI")] + [(8, "AI")] + [(9, "AI")] + [(1, "P1")] + [(2, "P1")] + [(3, "AI")] + [(4, "AI")] + [(5, "P1")] + [(6, "P1")] + [(7, "P1")] + [(3, "P1")] + [None] + [(4, "P1")] + [(8, "P1")] + [(9, "P1")] + [(10, "P1")]
         self.reset_ai()
         move, value = self.bh_ai.process_turn(board, 10, 10)
         self.assertEqual(value, 0)
 
     def test_minimax_win_over_tie(self):
-        board =  [None] + [(10, "P1")] * 2 + [(10, "AI")] * 7 + [(10, "AI")] * 2 + [(10, "AI")] * 4 + [None] + [(10, "P1")] * 4
+        board = [None] + [(1, "P1")] + [(2, "P1")] + [(5, "AI")] + [(6, "AI")] + [(7, "AI")] + [(8, "AI")] + [(9, "AI")] + [(1, "AI")] + [(2, "AI")] + [(3, "AI")] + [(4, "AI")] + [(5, "P1")] + [(6, "P1")] + [(7, "P1")] + [(3, "P1")] + [None] + [(4, "P1")] + [(8, "P1")] + [(9, "P1")] + [(10, "P1")]
         self.reset_ai()
         move, value = self.bh_ai.process_turn(board, 10, 10)
         self.assertEqual(value, 1000)
